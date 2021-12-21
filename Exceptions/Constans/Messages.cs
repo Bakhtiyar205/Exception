d31fdd48e9af9b7a4fdfdfa7db4d3867f429ff98
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2021-12-21 07:53:11 +0400

    exception

---
+++ Exceptions/Constans/Messages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions.Constans
+{
+    public class Messages
+    {
+        public static string PasswordMessage = "Password should be 6 charechters";
+    }
+}
